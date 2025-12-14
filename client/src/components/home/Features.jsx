3ea644321ba2759a749ea820a3fc6617904befe7
Manyata10 <22bcs10802@cuchd.in>
2025-12-14 16:03:26 +0530

    Initial Commit

--- client/src/components/home/Features.jsx
+++ client/src/components/home/Features.jsx
@@ -10,6 +10,7 @@ const Features = () => {
        <>
         <div className="flex items-center gap-2 text-sm text-green-800 bg-green-400/10 rounded-full px-6 py-1.5">
             <Zap width={14}></Zap>
+            
             <span>Simple Process</span>
         </div>
     
